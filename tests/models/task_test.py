df2fb3a16d424667f9293d95c8dc15a396f5c752
Oscar Andres Argueyo <oaargueyo@gmail.com>
2020-12-01 15:35:45 -0300

    Added tests for tasks

---
+++ tests/models/task_test.py
@@ -0,0 +1,32 @@
+import pytest
+
+from src.models.task import Task
+
+
+def test_attributes_tasks():
+
+    e_task = Task(
+        name="A",
+        deadline=1,
+        compute_time=100
+    )
+
+    assert e_task.name == "A"
+    assert e_task.deadline == 1
+    assert e_task.compute_time == 100
+    assert e_task.task_id != ""
+
+
+
+
+def test_repr_tasks():
+
+    e_task = Task(
+        name="A",
+        deadline=1,
+        compute_time=100
+    )
+
+    assert  e_task.__str__() == "A -> C:100 | D:1"
+
+
